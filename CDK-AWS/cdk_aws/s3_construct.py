b51c372a571ab8169aa18ca2908d2b395f2d8d41
Rupesh Mutha <rupesh.mutha@marketcast.com>
2024-05-15 18:18:38 +0530

    s3 construct updated

--- CDK-AWS/cdk_aws/s3_construct.py
+++ CDK-AWS/cdk_aws/s3_construct.py
@@ -3,7 +3,12 @@ from aws_cdk import aws_s3 as s3
 class S3Bucket:
     @staticmethod
     def create_s3_bucket(scope, deploy_env_type, config):
-        s3.Bucket(scope,
-            id="s3_resource_id",
-            bucket_name=config["s3_bucket_name"]
-        )
+        # Check if the bucket already exists
+        existing_bucket = s3.Bucket.from_bucket_name(scope, "ExistingBucket", config["s3_bucket_name"])
+
+        if existing_bucket is None:
+            # Create the bucket only if it doesn't already exist
+            s3.Bucket(scope,
+                id="s3_resource_id",
+                bucket_name=config["s3_bucket_name"]
+            )
